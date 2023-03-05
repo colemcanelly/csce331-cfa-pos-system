32e89ecbd57e388a3d2c6afc97ff596256261088
Cole McAnelly <cole.mcanelly3@gmail.com>
2023-03-05 17:54:35 -0600

    Updated db_gen.py to track inventory more accurately, thus making the simulation more realistic

--- phase2/db_gen.py
+++ phase2/db_gen.py
@@ -11,7 +11,6 @@ class Kiosk:
     def __repr__(self):
         return ",".join([str(val) for val in self.__dict__.values()])
 
-
 class MenuItem:
     def __init__(self, line: str) -> None:
         attr = line.split(',')
@@ -22,7 +21,6 @@ class MenuItem:
     def __repr__(self):
         return ",".join([str(val) for val in self.__dict__.values()])
 
-
 class Recipe:
     def __init__(self, line: str) -> None:
         attr = line.split(',')
@@ -31,13 +29,14 @@ class Recipe:
         self.portion_count: float = float(attr[2])
     
     def updateInventory(self, qty: float) -> None:
-        global DailyInv
-        onhand = next(filter(lambda itm : itm.ingredient == self.ingredient, DailyInv))
+        supply = next(filter(lambda itm : itm.ingredient == self.ingredient, SUPPLIES))
+        onhand = next(filter(lambda itm : itm.ingredient == self.ingredient, InvToday))
         onhand.qty_sold += (self.portion_count * qty)
+        if ((onhand.qty_sod + onhand.qty_new - onhand.qty_sold) > supply.threshold):
+            onhand.qty_new += supply.restock_quantity
     
     def __repr__(self):
         return ",".join([str(val) for val in self.__dict__.values()])
-
 
 class Supply:
     def __init__(self, line: str) -> None:
@@ -62,11 +61,11 @@ class Order:
             order_items:          dict[str,int]
             ) -> None:
         # hopefully this gets be reference, idk
-        global Kiosks
         kiosk = next(filter(lambda k : k.kiosk_id == kiosk_id, Kiosks))
         kiosk.curr_order_num += 1
 
         self.order_id:      int = id        # PK
+        assert (kiosk.curr_order_num < 1000), f"Order Number overflow: #{kiosk.curr_order_num}"
         self.order_num:     int = (kiosk.kiosk_id * 1000) + kiosk.curr_order_num
         self.order_date:    str = date
         self.order_time:    str = time
@@ -80,23 +79,20 @@ class Order:
     def __repr__(self):
         return ",".join([str(val) for val in self.__dict__.values()])
 
-
 class OrderItem:
     def __init__(self, id: int, order_entry: tuple[str,int]) -> None:
-        global Menu, Recipes
         self.order_id: int = id
         self.menu_item: str = str(order_entry[0])
         self.menu_item_qty: int = int(order_entry[1])
-        self.price: float = next(filter(lambda itm : itm.item_name == self.menu_item, Menu)).price
+        self.price: float = next(filter(lambda itm : itm.item_name == self.menu_item, MENU)).price
 
         map(
             lambda x: x.updateInventory(self.menu_item_qty), 
-            filter(lambda x : x.menu_item == self.menu_item, Recipes)
+            filter(lambda x : x.menu_item == self.menu_item, RECIPES)
             )
             
     def __repr__(self):
         return ",".join([str(val) for val in self.__dict__.values()])
-
 
 class Inventory:
     def __init__(
@@ -118,129 +114,154 @@ class Inventory:
     def __repr__(self):
         return ",".join([str(val) for val in self.__dict__.values()])
 
-
 #########################################################################################
 ##      GLOBAL VARS
 Kiosks:     list[Kiosk]
-Orders:     list[Order] = []
+MENU:       list[MenuItem]
+RECIPES:    list[Recipe]
+SUPPLIES:   list[Supply]
+Orders:     list[Order]     = []
 OrderItems: list[OrderItem] = []
-Menu:       list[MenuItem]
-Recipes:    list[Recipe]
-Supplies:   list[Supply]
-DailyInv:   list[Inventory] = []
-PrevDayInv:   list[Inventory] = []
+InvToday:   list[Inventory] = []
+InvHistory: list[Inventory] = []
 NAMES:      list[str] = ["Weston", "Cole", "Ryan", "Logan", "Prof Ritchie"]
 GAMEDAYS:   list[datetime.date] = [datetime.date(2022, 9, 10), datetime.date(2022, 9, 17)]
 order_count: int = 0
-DAILY_INVENTORY_FILE: str = "daily_inventory.csv"
-ORDERS_FILE: str = "orders.csv"
-ORDER_ITEMS_FILE: str = "order_items.csv"
 #########################################################################################
-##      METHODS
+##      FILE METHODS
 
-def fetchData(filename: str, constructor) -> list:
+def fetchTable(filename: str, constructor) -> list:
     fin = open(filename, 'r')
     data:list[str] = fin.readlines()
     return list(map(lambda line : constructor(line.replace('\n', '')), data))
+
+def loadTables(date: datetime.date) -> None:
+    kiosk_file: str = "./csv/kiosks.csv"
+    menu_file: str = "./csv/menu.csv"
+    recipe_file: str = "./csv/recipes.csv"
+    supply_file: str = "./csv/supply.csv"
+
+    global Kiosks, MENU, RECIPES, SUPPLIES, InvToday
+    Kiosks      = fetchTable(kiosk_file, lambda line : Kiosk(line))
+    MENU        = fetchTable(menu_file, lambda line : MenuItem(line))
+    RECIPES     = fetchTable(recipe_file, lambda line : Recipe(line))
+    SUPPLIES    = fetchTable(supply_file, lambda line : Supply(line))
+    InvToday    = list(map(lambda s : Inventory(str(date), s.ingredient, 0.0, 0.0, s.restock_quantity, 0.0), SUPPLIES))
+
+def unloadTables() -> None:
+    global Orders
+    orders_file: str = "./csv/orders.csv"
+    w_order_table = open(orders_file, 'w')
+    w_order_table.writelines(list(map(lambda s: str(s) + '\n', Orders)))
+    w_order_table.close()
+    global OrderItems
+    order_items_file: str = "./csv/order_items.csv"
+    w_order_items_table = open(order_items_file, 'w')
+    w_order_items_table.writelines(list(map(lambda s: str(s) + '\n', OrderItems)))
+    w_order_items_table.close()
+    global InvHistory
+    daily_inventory_file: str = "./csv/daily_inventory.csv"
+    w_daily_inventory = open(daily_inventory_file, 'w')
+    w_daily_inventory.writelines(list(map(lambda s: str(s) + '\n', InvHistory)))
+    w_daily_inventory.close()
+    
+#########################################################################################
+##      SIMULATION METHODS
 
 # Creates an order and returns the total price
 #       - Random customer name
 #       - Random kiosk used
 #       - Random number of order entries
-#       - Random Menu item selected
-#       - Random number of each Menu item
+#       - Random menu item selected
+#       - Random number of each menu item
 def generateSingleOrder(date: datetime.date, time: str) -> float:
-    global Kiosks, Orders, OrderItems, Menu, Recipes, Supplies, DailyInv, order_count
+    global Orders, OrderItems, order_count
     kiosk:  Kiosk   = random.choice(Kiosks) # random kiosk
     name:   str     = random.choice(NAMES)  # random customer
     total:  float   = 0.0
     items:  dict[str, int] = {}
-    for entry in range(random.randint(1, 5)):   # random number of order entries
-        item = random.choice(Menu)      # random item on the menu
-        number = random.randint(1,4)    # how many of that item
+    for _ in range(random.randint(1, 5)):   # random number of order entries
+        item = random.choice(MENU)      # random item on the menu
+        item_qty = random.randint(1,4)    # how many of that item
         if (item.combo):
-            items.update({"Waffle Potato Fries - Medium":number})
-            items.update({"Soft Drinks Medium":number})
-        total += (item.price * number)
-        items.update({item.item_name: number})
+            items.update({"Waffle Potato Fries - Medium":item_qty})
+            items.update({"Soft Drinks Medium":item_qty})
+        total += (item.price * item_qty)
+        items.update({item.item_name: item_qty})
     items.update({"To Go Bag":1})
     items.update({"Napkins":1})
     Orders.append(Order(order_count, str(date), time, total, name, kiosk.kiosk_id, items))
     order_count += 1
     return total
 
-# Simulate a day, return the total profit
-def simulateDay(date: datetime.date, min_profit: float) -> float:
-    global Supplies, DailyInv, DAILY_INVENTORY_FILE, PrevDayInv, Kiosks
-    DailyInv = list(map(lambda s : Inventory(str(date), s.ingredient, s.restock_quantity, 0.0, s.restock_quantity, 0.0), Supplies))
+def preOpening(date: datetime.date) -> None:
+    global InvToday
+    for item in InvToday:
+        item.date = str(date)
+        item.qty_sod = item.qty_eod
+        item.qty_eod = 0.0
+        item.qty_new = 0.0
+        item.qty_sold = 0.0
+
+def postClosing() -> None:
+    global Kiosks, InvToday, InvHistory
     for kiosk in Kiosks:
         kiosk.curr_order_num = 0
-    
-    min_profit += random.randint(0, int(min_profit / 10) + 1)   # random amount over the minimum
-    profit: float = 0.0
-    dateandtime = datetime.datetime(date.year, date.month, date.day, 8, 00) # 8:00 am
-    while (profit < min_profit):
-        dateandtime += datetime.timedelta(seconds=35)
-        profit += generateSingleOrder(date, str(dateandtime.time()))
-    w_daily_inventory = open(DAILY_INVENTORY_FILE, 'a')
-    w_daily_inventory.writelines(list(map(lambda s: str(s) + '\n', DailyInv)))
-    w_daily_inventory.close()
-    DailyInv = []
+    for item in InvToday:
+        item.qty_eod = item.qty_sod + item.qty_new - item.qty_sold
+    InvHistory.extend(InvToday)
 
+# Simulate a day, return the total profit
+def simulateDay(date: datetime.date, min_profit: float) -> float:
+    profit: float = 0.0
+    min_profit += (min_profit * (random.random() / 10))     # random amount over the minimum
+    date_and_time = datetime.datetime(date.year, date.month, date.day, 8, 00) # 8:00 am
+    preOpening(date)
+    while (profit < min_profit):
+        date_and_time += datetime.timedelta(seconds=35)
+        profit += generateSingleOrder(date, str(date_and_time.time()))
+    postClosing()
     return profit
 
 # Simulate a week
 def simulateWeek(start_date: datetime.date, min_profit: float) -> float:
-    global GAMEDAYS
-    days_per_week = 6
-    daily_profit: float = min_profit / days_per_week
+    business_days = 6
+    daily_profit: float = min_profit / business_days
     date: datetime.date = start_date
     profit: float = 0.0
-    for day in range(days_per_week):
+    for _ in range(business_days):
         if (date in GAMEDAYS):
             daily_profit *= 10
         profit += simulateDay(date, daily_profit)
         date += datetime.timedelta(days=1)
+    assert date == (start_date + datetime.timedelta(days=6)), "Incomplete weekly simulation"
     return profit
 
-def simulate(start_date: datetime.date, num_weeks: int, min_profit: float):
+def simulate(start_date: datetime.date, num_weeks: int, min_profit: float) -> None:
     weekly_profit: float = min_profit / num_weeks
     date: datetime.date = start_date
     profit: float = 0.0
     order_id = 0
 
-    print("Running Simulation")
-    for week in range(num_weeks):
+    print("\tRunning Simulation")
+    for _ in range(num_weeks):
         profit += simulateWeek(date, weekly_profit)
         date += datetime.timedelta(days=7)
-    print(f"Finshed. Profit = ${profit}")
+    print(f"\tFinshed.\n\tRevenue = ${profit}")
 
-def outputTables() -> None:
-    global ORDERS_FILE, ORDER_ITEMS_FILE, Orders, OrderItems
-    w_order_table = open(ORDERS_FILE, 'w')
-    w_order_table.writelines(list(map(lambda s: str(s) + '\n', Orders)))
-    w_order_table.close()
-    w_order_items_table = open(ORDER_ITEMS_FILE, 'w')
-    w_order_items_table.writelines(list(map(lambda s: str(s) + '\n', OrderItems)))
-    w_order_items_table.close()
 
-def main() -> None:
-    kiosk_file: str = "kiosks.csv"
-    menu_file: str = "menu.csv"
-    recipe_file: str = "recipes.csv"
-    supply_file: str = "supply.csv"
-
-    global Kiosks, Menu, Recipes, Supplies
-    Kiosks = fetchData(kiosk_file, lambda line : Kiosk(line))
-    Menu = fetchData(menu_file, lambda line : MenuItem(line))
-    Recipes = fetchData(recipe_file, lambda line : Recipe(line))
-    Supplies = fetchData(supply_file, lambda line : Supply(line))
-    
+def main() -> None:   
     start_date: datetime.date = datetime.date(2022, 2, 21)
     num_weeks = 52
     profit = 1000000.00
+    print("Beginning simulation . . . ")
+    print(f"\tBegin on {start_date},\n\tSimulate for {num_weeks} weeks,\n\tGenerate a minimum profit of ${profit}")
+    confirm = input("Press enter to confirm: ")
+    if (confirm != ""):
+        exit()
+    loadTables(start_date)
     simulate(start_date, num_weeks, profit)
-    outputTables()
+    unloadTables()
 
 if __name__ == "__main__":
     main()
