ad43565ce8e810496b64466fbe14fff18b4639ea
Logan Kettle <lgk24@me.com>
2023-03-07 17:18:22 -0600

    initial commit for customer GUI

---
+++ logan-customer-GUI/JFX_Setup/src/application/MainController.java
@@ -0,0 +1,25 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+public class MainController {
+
+    @FXML
+    private Button performOrder;
+
+    @FXML
+    private Button viewAnalysis;
+
+    @FXML
+    void analysisClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void orderClick(MouseEvent event) {
+
+    }
+
+}
